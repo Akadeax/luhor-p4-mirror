2232f86e022c4c15a7946cc623ff9e6b66a8b9dd
Patrick Nellessen <patrickqwertz@gmail.com>
2025-09-30 11:10:48 +0200

    add reworked health

--- Source/LuhorPrototype/AmbrosiaHealthComponent.h
+++ Source/LuhorPrototype/AmbrosiaHealthComponent.h
@@ -11,108 +11,118 @@
 /**
  * 
  */
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class LUHORPROTOTYPE_API UAmbrosiaHealthComponent final : public UHealthComponent ,public IComponentDependencies 
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class LUHORPROTOTYPE_API UAmbrosiaHealthComponent final : public UHealthComponent, public IComponentDependencies
 {
-	GENERATED_BODY()
-	COMPDEP_DECL()
-	
+    GENERATED_BODY()
+    COMPDEP_DECL()
+
 public:
-	UAmbrosiaHealthComponent()
-	{
-		PrimaryComponentTick.bCanEverTick = true;
-		MaxPoisonedAmbrosia = PoisonedAmbrosiaPerCharge;
-	};
-	virtual void BeginPlay() override;
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLastStandStarted);
-	UPROPERTY(BlueprintAssignable) FOnLastStandStarted OnLastStandStarted;
+    UAmbrosiaHealthComponent()
+    {
+        PrimaryComponentTick.bCanEverTick = true;
+        MaxPoisonedAmbrosia = PoisonedAmbrosiaPerCharge;
+    }
 
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLastStandEnded);
-	UPROPERTY(BlueprintAssignable) FOnLastStandStarted OnLastStandEnded;
-	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    virtual void BeginPlay() override;
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLastStandStarted);
+    UPROPERTY(BlueprintAssignable)
+    FOnLastStandStarted OnLastStandStarted;
 
-	virtual void Damage(float Amount) override;
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLastStandEnded);
+    UPROPERTY(BlueprintAssignable)
+    FOnLastStandStarted OnLastStandEnded;
 
-	virtual void Heal(float Amount) override;
-	
-	UFUNCTION(BlueprintCallable)
-	void SiphonAmbrosia(float Amount);
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	UFUNCTION(BlueprintCallable)
-	bool TrySpendSpecialCharge();
-	
-	UFUNCTION(BlueprintCallable)
-	void AddSpecialChargeSlots(const int Amount = 1);
+    virtual void Damage(float Amount) override;
 
-	UFUNCTION(BlueprintCallable)
-	float GetCurrentPoisonedAmbrosia() const;
-	
-	UFUNCTION(BlueprintCallable)
-	float GetCurrentPoisonedAmbrosiaPercentage() const;
-	
-	UFUNCTION(BlueprintCallable)
-	int GetSpecialAttackCharges() const;
+    virtual void Heal(float Amount) override;
 
-	UFUNCTION(BlueprintCallable) void StartDrain() { DoDrain = true; }
-	UFUNCTION(BlueprintCallable) void EndDrain()   { DoDrain = false; }
-	
-	UFUNCTION(BlueprintCallable)
-	bool IsDead() const { return HasDied; }
-	
-	UFUNCTION(BlueprintCallable)
-	void ResetBools();
+    UFUNCTION(BlueprintCallable)
+    void SiphonAmbrosia(float Amount);
 
-	void SetPoisonSiphonPercent(float Val) { PoisonSiphonPercent = Val; }
-	float GetPoisonSiphonPercent() const { return PoisonSiphonPercent; }
+    UFUNCTION(BlueprintCallable)
+    bool TrySpendSpecialCharge();
 
-	void SetSiphonPercentage(float Val) { SiphonPercentage = Val; }
-	float GetSiphonPercentage() const { return SiphonPercentage; }
-	
-protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia|Siphon")
-	float SiphonPercentage{ 52.f };
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia|Special Attack")
-	float PoisonedAmbrosiaPerCharge{ 25.f };
-	
-	UPROPERTY(BlueprintReadOnly, Category="Ambrosia|Special Attack")
-	float MaxPoisonedAmbrosia{ PoisonedAmbrosiaPerCharge };
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia|Special Attack")
-	float CurrentPoisonedAmbrosia{ 25.f };
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia|Special Attack")
-	float TargetPoisonedAmbrosia{ 0.f };
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia|Special Attack")
-	float PoisonedAmbrosiaFillSpeed{ 20.f };
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia|Special Attack")
-	int StartingCharges{ 3 };
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Ambrosia|Special Attack", meta = (ToolTip = "The percentage of ambrosia the player needs to have before putting a part of its ambrosia gain towards filling the special attack meter"))
-	float SpecialChargeCutoff{ 0.66f };
+    UFUNCTION(BlueprintCallable)
+    void AddSpecialChargeSlots(const int Amount = 1);
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Ambrosia")
-	float PassiveAmbrosiaDrain{ 2.f };
+    UFUNCTION(BlueprintCallable)
+    float GetCurrentPoisonedAmbrosia() const;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Ambrosia")
-	float TargetHealth{0.f};
+    UFUNCTION(BlueprintCallable)
+    float GetCurrentPoisonedAmbrosiaPercentage() const;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ambrosia")
-	float AmbrosiaFillSpeed{ 5.f };
+    UFUNCTION(BlueprintCallable)
+    int GetSpecialAttackCharges() const;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Ambrosia")
-	float StartingAmbrosia{ 40.f };
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Ambrosia" , meta = (ToolTip = "How much ambrosia the player needs to gather to get out of the last stand mode"))
-	float LastStandEndCutoff{ 5.f };
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Ambrosia|Special Attack")
-	float PoisonSiphonPercent{ 0.15f };
-	
-	UPROPERTY() UUpgradesComponent* UpgradesComponent{};
-	
-	bool DoDrain{ false };
-	bool InLastStand{ false };
-	bool HasDied{ false };
+    UFUNCTION(BlueprintCallable)
+    void StartDrain() { DoDrain = true; }
+
+    UFUNCTION(BlueprintCallable)
+    void EndDrain() { DoDrain = false; }
+
+    UFUNCTION(BlueprintCallable)
+    bool IsDead() const { return HasDied; }
+
+    UFUNCTION(BlueprintCallable)
+    void ResetBools();
+
+    void SetPoisonSiphonPercent(float Val) { PoisonSiphonPercent = Val; }
+
+    float GetPoisonSiphonPercent() const { return PoisonSiphonPercent; }
+
+    void SetSiphonPercentage(float Val) { SiphonPercentage = Val; }
+
+    float GetSiphonPercentage() const { return SiphonPercentage; }
+
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Siphon")
+    float SiphonPercentage{ 52.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    float PoisonedAmbrosiaPerCharge{ 25.f };
+
+    UPROPERTY(BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    float MaxPoisonedAmbrosia{ PoisonedAmbrosiaPerCharge };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    float CurrentPoisonedAmbrosia{ 25.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    float TargetPoisonedAmbrosia{ 0.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    float PoisonedAmbrosiaFillSpeed{ 20.f };
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    int StartingCharges{ 3 };
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack", meta = (ToolTip = "The percentage of ambrosia the player needs to have before putting a part of its ambrosia gain towards filling the special attack meter"))
+    float SpecialChargeCutoff{ 0.66f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia")
+    float PassiveAmbrosiaDrain{ 2.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia")
+    float TargetHealth{ 0.f };
+
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia")
+    float AmbrosiaFillSpeed{ 5.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia")
+    float StartingAmbrosia{ 40.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia", meta = (ToolTip = "How much ambrosia the player needs to gather to get out of the last stand mode"))
+    float LastStandEndCutoff{ 5.f };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ambrosia|Special Attack")
+    float PoisonSiphonPercent{ 0.15f };
+
+    UPROPERTY()
+    UUpgradesComponent* UpgradesComponent{};
+
+    bool DoDrain{ false };
+    bool InLastStand{ false };
+    bool HasDied{ false };
 };
